ad75b05e3aa75219ac0fef237a6a20032028334f
d.shmakov <d.shmakov@rabota.ru>
2020-08-04 00:22:48 +1000

    first: counet nuxt

---
+++ pages/counter.vue
@@ -0,0 +1,85 @@
+<template>
+  <section class="container">
+    <div class="counter-block">
+        <p class="number">{{ counter }}</p>
+        <div class="button-block">
+          <button 
+            v-on:click="plusClick()"
+            class="button"
+          >
+            +
+          </button>
+          <button 
+            v-on:click="minusClick()"
+            class="button"
+          >
+            -
+          </button>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+
+  data: () => ({
+    counter: 0
+  }),
+
+  methods: {
+    plusClick() {
+      this.counter += 1;
+    },
+    minusClick() {
+      this.counter > 0 ?
+      this.counter -= 1:
+      this.counter;
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.counter-block {
+  display: grid;
+  padding: 20px;
+  width: 200px;
+  height: 300px;
+  border: 1px solid lightskyblue;
+}
+
+.number {
+  font-size: 100px;
+}
+
+.button-block{
+  display: grid;
+  grid-auto-flow: column;
+  margin: auto;
+  grid-column-gap: 25px;
+}
+
+.button {
+  grid-column-gap: 10px;
+  font-size: 40px;
+  width: 50px;
+  background: lightskyblue;
+  color: white;
+  outline: none;
+  border:0;
+  border-radius: 50px;
+  cursor: pointer;
+}
+
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <h1>My project in progress </h1>
+    <hr>
+    <nuxt-link to="/counter">
+      link first project
+    </nuxt-link>
+  </section>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>
+
+https://github.com/Shpakovich/JS-project.git
